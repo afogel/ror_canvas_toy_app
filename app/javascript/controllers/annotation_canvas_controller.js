2015f54edd12360c98d79c85c59be60fe04407f6
Ariel Fogel <fogeltine@gmail.com>
2022-12-06 16:52:42 +0200

    add the canvas to the DOM once the stimulus controller connects

--- app/javascript/controllers/annotation_canvas_controller.js
+++ app/javascript/controllers/annotation_canvas_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import Konva from 'konva';
 
 // Connects to data-controller="annotation-canvas"
 export default class extends Controller {
+  static targets = ['canvas'];
   connect() {
+    this.stage = new Konva.Stage({
+      container: 'annotation-container',
+      width: this.canvasTarget.clientWidth,
+      height: this.canvasTarget.clientHeight,
+    });
+  }
+
+  disconnect() {
+    // clean up the canvas when navigating away
+    this.stage.destroy();
   }
 }
