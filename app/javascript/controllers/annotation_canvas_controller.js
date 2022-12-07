ac6118328d0e10e3f148d4ad7480d00a830ac547
Ariel Fogel <fogeltine@gmail.com>
2022-12-07 12:08:29 +0200

    paint image to the canvas

--- app/javascript/controllers/annotation_canvas_controller.js
+++ app/javascript/controllers/annotation_canvas_controller.js
@@ -1,15 +1,36 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 import Konva from 'konva';
 
 // Connects to data-controller="annotation-canvas"
 export default class extends Controller {
   static targets = ['canvas'];
+
+  static values = { imageUrl: String };
+
   connect() {
     this.stage = new Konva.Stage({
       container: 'annotation-container',
       width: this.canvasTarget.clientWidth,
       height: this.canvasTarget.clientHeight,
     });
+
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer);
+
+    const currentImage = new Image();
+    currentImage.src = this.imageUrlValue;
+    currentImage.onload = () => {
+      this.image = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: currentImage,
+        width: this.stage.width(),
+        height: this.stage.height(),
+      });
+
+      // add the shape to the layer
+      this.layer.add(this.image);
+    };
   }
 
   disconnect() {
