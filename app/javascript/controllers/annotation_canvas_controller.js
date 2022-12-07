817900fa1004b260434fd1d42a4d248dff334172
Ariel Fogel <fogeltine@gmail.com>
2022-12-07 15:58:14 +0200

    Add new polygon when instantiating controller along with landmarks target

--- app/javascript/controllers/annotation_canvas_controller.js
+++ app/javascript/controllers/annotation_canvas_controller.js
@@ -3,7 +3,7 @@ import Konva from 'konva';
 
 // Connects to data-controller="annotation-canvas"
 export default class extends Controller {
-  static targets = ['canvas'];
+  static targets = ['canvas', 'landmarks'];
 
   static values = { imageUrl: String };
 
@@ -16,6 +16,8 @@ export default class extends Controller {
 
     this.layer = new Konva.Layer();
     this.stage.add(this.layer);
+    this.group = new Konva.Group({ name: 'annotation' });
+    this.layer.add(this.group);
 
     const currentImage = new Image();
     currentImage.src = this.imageUrlValue;
@@ -31,6 +33,17 @@ export default class extends Controller {
       // add the shape to the layer
       this.layer.add(this.image);
     };
+    this.polygon = new Konva.Line({
+      stroke: '#00F1FF',
+      strokeWidth: 3,
+      closed: false,
+      fill: 'rgb(140,30,255,0.25)',
+      id: 'annotation',
+      name: 'polygon',
+    });
+
+    this.stage.add(this.polygon);
+    this.polygon.draw();
   }
 
   disconnect() {
